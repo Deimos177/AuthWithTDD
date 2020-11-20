6369606d9514ee1a6840f293ac3dbedd09ba18cb
Deimos177 <brucevieira.2009@gmail.com>
2020-11-20 19:14:56 -0300

    TDD app tests finished

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -15,7 +15,7 @@ describe('Authentication', () => {
             .post("/sessions")
             .send({
                 email: user.email,
-                password: "123123"
+                password: user.password
             })
         expect(response.status).toBe(200)
     })
@@ -32,6 +32,18 @@ describe('Authentication', () => {
         expect(response.status).toBe(401)
     })
 
+    it('should not authenticate with invalid email', async () => {
+        const user = await factory.create('User')
+
+        const response = await request(app)
+            .post("/sessions")
+            .send({
+                email: "tralala@trlala.com",
+                password: user.password
+            })
+        expect(response.status).toBe(401)
+    })
+
     it('should return jwt token when authenticated', async () => {
         const user = await factory.create('User')
 
@@ -39,7 +51,7 @@ describe('Authentication', () => {
             .post("/sessions")
             .send({
                 email: user.email,
-                password: "123123"
+                password: user.password
             })
         expect(response.body).toHaveProperty("token")
     })
@@ -49,8 +61,26 @@ describe('Authentication', () => {
         const response = await request(app)
             .get("/dashboard")
             .set('Authorization', `Bearer ${user.generateToken()}`)
-        expect(response.status).toBe(200)
+        expect(response.status).toBe(401)
     })
+
+    it('Should not be able to acess private routes without jwt token', async () => {
+        const user = await factory.create('User')
+
+        const response = await request(app)
+            .get("/dashboard")
+
+        expect(response.status).toBe(401)
+    })
+    it('Should not be able to acess private routes with invalid jwt token', async () => {
+        const user = await factory.create('User')
+
+        const response = await request(app)
+            .get("/dashboard")
+            .set('Authorization', `Bearer 1234000`)
+        expect(response.status).toBe(401)
+    })
+
 })
 
 /*Describe = categoria de testes*/
---
+++ src/app/middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+const { promisify } = require('util')
+
+module.exports = async (req, res, next) =>{
+    const authHeader = req.headers.Authorization
+
+    if(!authHeader){
+        return res.status(401).json({ message: 'Token not provided' })
+    }
+
+    const [, token] = authHeader.split(' ')
+
+    try{
+        const decoded = await promisify(jwt.verify)(token, process.env.APP_SECRET)
+
+        req.userId = decoded.id
+
+        return next(), res.status(200).send()
+    }catch(err){
+        return res.status(401).json({ message: 'Token invalid' })
+    }
+}
